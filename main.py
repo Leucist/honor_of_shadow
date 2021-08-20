4a357dc812a9b76a87ccddf22c3516559894063f
Leucist <vlad2ka@tut.by>
2021-08-21 01:07:34 +0300

    Added collisions and screen borders

--- main.py
+++ main.py
@@ -15,15 +15,17 @@ clock = pygame.time.Clock()
 # window_width, window_height = screen_width - 10, screen_height - 50
 # window = pygame.display.set_mode((window_width,window_height))
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+global display_width
 
 
 class GameCharacter:
     def __init__(self, coords, ch_type):
         self.animation_database = {}
         self.animation_frames = {}
+        self.hit_list = []
         self.mode = 'idle'
         self.anim_frame = 0
-        self.moving = 0
+        self.moving = [0, 0]
         self.flip = False
         self.name = ch_type
         self.sprite = pygame.image.load('source/animations/' + self.name + '/idle/idle_0.png').convert_alpha()
@@ -33,13 +35,40 @@ class GameCharacter:
             self.weapon_damage = data[self.name][1]
             self.weapon_cd = data[self.name][2]  # weapon cool down
             self.speed = data[self.name][3]  # [walk speed, run speed]
+            self.speed['idle'] = 0
             animset = data[self.name][4]
             for mode in animset:
                 self.load_animation(mode, animset[mode])
         self.rect = pygame.Rect(coords[0], coords[1], self.sprite.get_width(), self.sprite.get_height())
 
-    def move(self, direction):
-        self.rect.x += direction * self.speed[self.mode]
+    def move(self, collidable_objects):
+        self.rect.x += self.moving[0] * self.speed[self.mode]
+        self.check_collision(collidable_objects)
+        if self.rect.x <= 0:
+            self.rect.x = 0
+            self.mode = 'idle'
+        right_border = display_width - self.sprite.get_width()
+        if self.rect.x >= right_border:
+            self.rect.x = right_border
+            self.mode = 'idle'
+        for obj in self.hit_list:
+            if self.moving[0] > 0:
+                self.rect.right = obj.left
+                self.mode = 'idle'
+            elif self.moving[0] < 0:
+                self.rect.left = obj.right
+                self.mode = 'idle'
+        self.rect.y += self.moving[1] * self.speed[self.mode]
+        self.check_collision(collidable_objects)
+        for obj in self.hit_list:
+            if self.moving[1] > 0:
+                self.rect.bottom = obj.top
+                self.mode = 'idle'
+            elif self.moving[1] < 0:
+                self.rect.top = obj.bottom
+                self.mode = 'idle'
+            # self.speed[1] = 0
+
 
     def load_animation(self, mode, frame_durations):
         animation_frame_data = []
@@ -60,6 +89,14 @@ class GameCharacter:
             self.anim_frame = 0
         self.sprite = self.animation_frames[self.animation_database[self.mode][self.anim_frame]]
 
+    def check_collision(self, collidable_objects):
+        self.hit_list = []
+        for obj in collidable_objects:
+            # if obj == self.rect:
+            #     continue
+            if self.rect.colliderect(obj):
+                self.hit_list.append(obj)
+
 
 def start():
     # display = pygame.Surface((600, 400))
@@ -69,6 +106,8 @@ def start():
 
 
 def main_menu():
+    global display_width
+    display_width = 600
     with open("settings.json", "r", encoding="UTF-8") as settings_file:
         settings = json.loads(settings_file.read())
     info = pygame.display.Info()
@@ -112,9 +151,12 @@ def main_menu():
     chain_down = pygame.Rect(333, 137, chain_down_image.get_width(), chain_down_image.get_height())
     images = [button_start_image, button_load_image, button_settings_image, button_exit_image, chain_image,
               chain_down_image]
-    objects = [button_start, button_load, button_settings, button_exit, chain, chain_down]
+    game_objects = [button_start, button_load, button_settings, button_exit, chain, chain_down]
+    collidable_objects = [button_start, button_load, button_settings, button_exit]
+    collidable_objects.append(pygame.Rect(0, 379, 600, 1))      # floor
+    collidable_objects.append(pygame.Rect(220, 337, 165, 43))   # stairs
     characters = [purple_ninja, green_ninja]
-    LIGHT_DISTANCE = [50, 50,50, 51,51, 51, 49, 49, 49] * 4
+    LIGHT_DISTANCE = [50, 50, 50, 51, 51, 51, 49, 49, 49] * 4
     green_ninja.anim_frame = 0
     purple_ninja.anim_frame = 0
     running = True
@@ -128,12 +170,11 @@ def main_menu():
 
         light = []
         # Probably gonna remake it in order it will be easier to treat torches as single objects
-        purple_torch = [purple_ninja.rect.x + 3 + 40 * purple_ninja.flip, purple_ninja.rect.y + 12]
-        green_torch = [green_ninja.rect.x + 3 + 40 * green_ninja.flip, green_ninja.rect.y + 12]
+        purple_torch = [purple_ninja.rect.x + 6 + 44 * purple_ninja.flip, purple_ninja.rect.y + 12]
+        green_torch = [green_ninja.rect.x + 6 + 44 * green_ninja.flip, green_ninja.rect.y + 12]
         for i in range(-LIGHT_DISTANCE[purple_ninja.anim_frame], LIGHT_DISTANCE[purple_ninja.anim_frame] + 1):
-            # x_ch = (LIGHT_DISTANCE + i)**2  # [0, 2LD]
             x_ch = ((LIGHT_DISTANCE[purple_ninja.anim_frame] - i) * (
-                        LIGHT_DISTANCE[purple_ninja.anim_frame] + i)) ** 0.5
+                    LIGHT_DISTANCE[purple_ninja.anim_frame] + i)) ** 0.5
             w_ch = x_ch * 2
             light.append(pygame.Rect(purple_torch[0] - x_ch, purple_torch[1] + i,
                                      w_ch, 1))
@@ -143,11 +184,12 @@ def main_menu():
         for rect in light:
             display.blit(light_background, rect, rect)
 
-        for image, obj in zip(images, objects):
+        for image, obj in zip(images, game_objects):
             display.blit(image, obj)
+
         for character in characters:
-            if character.moving != 0:
-                character.move(character.moving)
+            if character.moving[0] != 0:
+                character.move(collidable_objects)
             character.change_frame()
             character.anim_frame += 1
             display.blit(pygame.transform.flip(character.sprite, character.flip, False), character.rect)
@@ -160,21 +202,21 @@ def main_menu():
                     running = False
                 if event.key == K_RIGHT:
                     green_ninja.flip = False
-                    green_ninja.moving = 1
+                    green_ninja.moving[0] = 1
                     green_ninja.anim_frame = 0
                     green_ninja.mode = 'walk'
                 if event.key == K_LEFT:
                     green_ninja.flip = True
-                    green_ninja.moving = -1
+                    green_ninja.moving[0] = -1
                     green_ninja.anim_frame = 0
                     green_ninja.mode = 'walk'
             if event.type == KEYUP:
                 if event.key == K_RIGHT:
-                    green_ninja.moving = 0
+                    green_ninja.moving[0] = 0
                     green_ninja.anim_frame = 0
                     green_ninja.mode = 'idle'
                 if event.key == K_LEFT:
-                    green_ninja.moving = 0
+                    green_ninja.moving[0] = 0
                     green_ninja.anim_frame = 0
                     green_ninja.mode = 'idle'
         surf = pygame.transform.scale(display, win_size)
